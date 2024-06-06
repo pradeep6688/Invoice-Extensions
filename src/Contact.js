2807b20060f0e2e553964bd210f4054569e78668
Pradeep Medarametla <pradeepmedarametla@Pradeeps-Air.attlocal.net>
2024-06-06 12:35:46 -0500

    updated tests

--- src/Contact.js
+++ src/Contact.js
@@ -1,99 +1,126 @@
-const express = require('express');
 const os = require('os');
-const cors = require('cors');
-
-const app = express();
-const port = 3001;
-
-app.use(cors());
-
-// Function to get local IP address
-const getLocalIPAddress = () => {
-  const interfaces = os.networkInterfaces();
-  for (const interfaceName in interfaces) {
-    const addresses = interfaces[interfaceName];
-    for (const address of addresses) {
-      if (address.family === 'IPv4' && !address.internal) {
-        return address.address;
-      }
-    }
-  }
-  return 'Unable to determine local IP address';
-};
-
-// Endpoint to get local IP address
-app.get('/api/ip', (req, res) => {
-  const ipAddress = getLocalIPAddress();
-  res.json({ ip: ipAddress });
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
-
-
-
-
-const express = require('express');
-const os = require('os');
-const cors = require('cors');
 const { exec } = require('child_process');
+const logger = require('your-logger-module'); // Replace with your actual logger
 
-const app = express();
-const port = 3001;
+const getSystemInfo = (req, res) => {
+  const systemInfo = {
+    operatingSystem: os.platform(),
+    osVersion: os.release(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+    cpuArchitecture: os.arch(),
+    cpuInfo: os.cpus(),
+    homeDirectory: os.homedir(),
+    hostName: os.hostname(),
+    networkInterfaces: os.networkInterfaces(),
+  };
 
-app.use(cors());
+  logger.trace('networkutilities :: SystemInfo :: Gathering system information', systemInfo);
 
-const isCitrixEnvironment = () => {
-  // Check for specific environment variables
-  if (process.env.CTX_CLIENT_NAME) {
-    return true;
-  }
+  checkCitrixProcesses((isCitrixProcess) => {
+    const isCitrixEnv = checkCitrixEnvVariables();
+    systemInfo.isCitrix = isCitrixProcess || isCitrixEnv;
 
-  // Check for Citrix-related processes
-  if (os.platform() === 'win32') {
-    // Command to list processes on Windows
-    return new Promise((resolve) => {
-      exec('tasklist', (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          resolve(false);
-          return;
-        }
-        if (stdout.includes('wfcrun32.exe') || stdout.includes('Receiver.exe')) {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+    logger.info('networkutilities :: SystemInfo :: Citrix check completed', { isCitrix: systemInfo.isCitrix });
+
+    res.status(200).send({
+      message: 'System information gathered successfully',
+      systemInfo,
     });
-  } else if (os.platform() === 'linux' || os.platform() === 'darwin') {
-    // Command to list processes on Linux/macOS
-    return new Promise((resolve) => {
-      exec('ps aux', (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          resolve(false);
-          return;
-        }
-        if (stdout.includes('wfcrun32') || stdout.includes('Receiver')) {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
-    });
-  }
-
-  return false;
+  });
 };
 
-app.get('/api/environment', async (req, res) => {
-  const isCitrix = await isCitrixEnvironment();
-  res.json({ isCitrix });
-});
+// Function to check for Citrix processes
+function checkCitrixProcesses(callback) {
+  exec('tasklist', (error, stdout, stderr) => {
+    if (error) {
+      logger.error('networkutilities :: SystemInfo :: Error executing tasklist command', { error });
+      callback(false);
+      return;
+    }
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+    const citrixProcesses = ['wfica32.exe', 'Receiver.exe', 'concentr.exe'];
+    const isCitrix = citrixProcesses.some(process => stdout.includes(process));
+    logger.trace('networkutilities :: SystemInfo :: Citrix process check', { isCitrix });
+
+    callback(isCitrix);
+  });
+}
+
+// Function to check for Citrix environment variables
+function checkCitrixEnvVariables() {
+  const citrixEnvVariables = ['CTX_SESSION', 'CTX_EMULATE'];
+  const isCitrixEnv = citrixEnvVariables.some(env => process.env[env] !== undefined);
+
+  logger.trace('networkutilities :: SystemInfo :: Citrix environment variable check', { isCitrixEnv });
+  return isCitrixEnv;
+}
+
+const systemInfoExport = {
+  getSystemInfo,
+};
+
+module.exports = systemInfoExport;
+const os = require('os');
+const { exec } = require('child_process');
+const logger = require('your-logger-module'); // Replace with your actual logger
+
+const getSystemInfo = (req, res) => {
+  const systemInfo = {
+    operatingSystem: os.platform(),
+    osVersion: os.release(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+    cpuArchitecture: os.arch(),
+    cpuInfo: os.cpus(),
+    homeDirectory: os.homedir(),
+    hostName: os.hostname(),
+    networkInterfaces: os.networkInterfaces(),
+  };
+
+  logger.trace('networkutilities :: SystemInfo :: Gathering system information', systemInfo);
+
+  checkCitrixProcesses((isCitrixProcess) => {
+    const isCitrixEnv = checkCitrixEnvVariables();
+    systemInfo.isCitrix = isCitrixProcess || isCitrixEnv;
+
+    logger.info('networkutilities :: SystemInfo :: Citrix check completed', { isCitrix: systemInfo.isCitrix });
+
+    res.status(200).send({
+      message: 'System information gathered successfully',
+      systemInfo,
+    });
+  });
+};
+
+// Function to check for Citrix processes
+function checkCitrixProcesses(callback) {
+  exec('tasklist', (error, stdout, stderr) => {
+    if (error) {
+      logger.error('networkutilities :: SystemInfo :: Error executing tasklist command', { error });
+      callback(false);
+      return;
+    }
+
+    const citrixProcesses = ['wfica32.exe', 'Receiver.exe', 'concentr.exe'];
+    const isCitrix = citrixProcesses.some(process => stdout.includes(process));
+    logger.trace('networkutilities :: SystemInfo :: Citrix process check', { isCitrix });
+
+    callback(isCitrix);
+  });
+}
+
+// Function to check for Citrix environment variables
+function checkCitrixEnvVariables() {
+  const citrixEnvVariables = ['CTX_SESSION', 'CTX_EMULATE'];
+  const isCitrixEnv = citrixEnvVariables.some(env => process.env[env] !== undefined);
+
+  logger.trace('networkutilities :: SystemInfo :: Citrix environment variable check', { isCitrixEnv });
+  return isCitrixEnv;
+}
+
+const systemInfoExport = {
+  getSystemInfo,
+};
+
+module.exports = systemInfoExport;
